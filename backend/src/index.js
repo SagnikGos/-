8f370ecc0f3f8e3a9fd0d784a90a8bc9ac68c567
SagnikGos <sagnik.gos06@gmail.com>
2024-11-23 20:28:49 +0530

    2nd push

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, ()=> {
-    console.log("server is running")
+app.listen(PORT, ()=> {
+    console.log("server is running @ " + PORT);
+    connectDB();
 });
 
-// 7Cz6s0NYX7D5yyGa
