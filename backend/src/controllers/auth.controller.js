01a881110f277694f4a680df39c958f48897f39f
SagnikGos <sagnik.gos06@gmail.com>
2024-11-30 00:55:44 +0530

    Backend Complete

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -115,3 +115,12 @@ export const updateProfile = async (res,req) => {
         })     
     }
 };
+
+export const checkAuth = (req,res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error"});
+    }
+}
